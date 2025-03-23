3b6f93a4fc414f773af1a4923a0aa76fc6017e98
spoingus car <161335472+spoinguscar@users.noreply.github.com>
2025-03-23 13:24:03 +0530

    Add files via upload

---
+++ 5-project-filemetadata/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+var upload = multer({
+  dest: 'uploads/'
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    res.json({
+      error: 'No file uploaded'
+    })
+  } else {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  }
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
